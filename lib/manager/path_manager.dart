1aff7b491e4c491e510df55e037c05ab0b38645b
Alex <nomadalexinjp@gmail.com>
2025-05-09 08:36:18 +0800

    Change epub download path.

--- lib/manager/path_manager.dart
+++ lib/manager/path_manager.dart
@@ -1,25 +1,24 @@
 import 'dart:io';
 
 import 'package:path_provider/path_provider.dart';
+import 'package:downloadsfolder/downloadsfolder.dart';
 
 final pathManager = _PathManager();
 
 class _PathManager {
   late String externalStorageDirectory;
+  late String downloadsDirectory;
 
-  final _epubDownloadPath = '/downloads/epub';
+  final _epubDownloadPath = '/epub';
   final _loggerPath = '/log';
 
   _PathManager();
 
   Future<void> init() async {
-    await _getExternalStorageDirectory();
-  }
-
-  Future<void> _getExternalStorageDirectory() async {
-    final directory = await getExternalStorageDirectory();
-    if (directory == null) throw Exception('no external storage');
-    externalStorageDirectory = directory.path;
+    final [dir1, dir2] = await Future.wait([getExternalStorageDirectory(), getDownloadDirectory()]);
+    if (dir1 == null || dir2 == null) throw Exception('init path manager error');
+    externalStorageDirectory = dir1.path;
+    downloadsDirectory = dir2.path;
   }
 
   List<String> getLogPathList() {
@@ -33,8 +32,8 @@ class _PathManager {
     return logFiles.map((file) => file.path).toList();
   }
 
-  /// externalStorageDirectory/downloads/epub
-  String get epubDownloadPath => externalStorageDirectory + _epubDownloadPath;
+  /// downloadsDirectory/epub
+  String get epubDownloadPath => downloadsDirectory + _epubDownloadPath;
 
   String get loggerPath => externalStorageDirectory + _loggerPath;
 }
